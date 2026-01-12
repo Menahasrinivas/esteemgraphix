6fbf532ae3b935e9b750e08752e7871174f0cd7c
menahasrinivas <menahasrinivas@gmail.com>
2026-01-12 20:09:31 +0530

    add

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  // Load Header
   fetch("includes/header.html")
     .then(res => res.text())
     .then(data => {
-      document.getElementById("header").innerHTML = data;
+      const header = document.getElementById("header");
+      if (header) {
+        header.innerHTML = data;
+      }
+    });
+
+  // Load Footer
+ 
+  fetch("includes/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+
+      // ✅ year set AFTER footer loaded
+      const yearEl = document.getElementById("year");
+      if (yearEl) {
+        yearEl.textContent = new Date().getFullYear();
+      }
     });
 });
+
